a070469a71194bba2ed8f9ab3da3b78707a71c15
Josh Harris <josh.harris@taxcalc.com>
2017-07-06 15:17:44 +0100

    Fixed packet sending using the wrong length

--- Bolt/Connection/ClientBridge.cs
+++ Bolt/Connection/ClientBridge.cs
@@ -38,7 +38,8 @@ namespace Bolt.Connection
                             TerrariaPacket deserializedPacket = TerrariaPacket.Deserialize(br);
                             Console.WriteLine ("[Bolt] [{0}] Received from client: {1}", Thread.CurrentThread.Name, deserializedPacket);
                             Console.WriteLine ("[Bolt] [{0}] Sent to server: {1}", Thread.CurrentThread.Name, deserializedPacket);
-                            conn.CurrentServer.output.Write (deserializedPacket.ToArray(), 0, deserializedPacket.GetLength());
+                            byte[] buffer = deserializedPacket.ToArray();
+                            conn.CurrentServer.output.Write (buffer, 0, buffer.Length);
                         }
 
                     }
--- Bolt/Connection/ServerBridge.cs
+++ Bolt/Connection/ServerBridge.cs
@@ -39,8 +39,9 @@ namespace Bolt.Connection
                         {
                             TerrariaPacket deserializedPacket = TerrariaPacket.Deserialize(br);
                             Console.WriteLine ("[Bolt] [{0}] Received from server: {1}", Thread.CurrentThread.Name, deserializedPacket);
-                            Console.WriteLine ("[Bolt] [{0}] Sent to client: {1}", Thread.CurrentThread.Name, deserializedPacket);
-                            ClientConnection.output.Write (deserializedPacket.ToArray(), 0, deserializedPacket.GetLength());
+                            Console.WriteLine("[Bolt] [{0}] Sent to client: {1}", Thread.CurrentThread.Name, deserializedPacket);
+                            byte[] buffer = deserializedPacket.ToArray();
+                            ClientConnection.output.Write(buffer, 0, buffer.Length);
                         }
                     }
                     else
